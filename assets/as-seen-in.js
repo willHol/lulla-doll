5694f1f8f7fc0458c3f75583e90da19927d0e126
William Holmes <thenecroyeti@gmail.com>
2023-08-26 00:10:25 +0700

    LULLA-NONE: JS for as-seen-in

---
+++ assets/as-seen-in.js
@@ -0,0 +1,48 @@
+const SLIDER_ACTIVATION_WIDTH = 768;
+
+class AsSeenIn extends HTMLElement {
+  constructor() {
+    super();
+
+    if (this.dataset['swiperEnabled']) {
+      this.setupSwiper();
+    }
+  }
+
+  setupSwiper() {
+    waitForProperty('Swiper', (success) => {
+      if (!success) {
+        throw new Error('Swiper was not loaded in time');
+      }
+
+      if (window.innerWidth >= SLIDER_ACTIVATION_WIDTH) {
+        return;
+      }
+
+      this.classList.add('slider-active');
+
+      this.swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true, // TODO: Make configurable
+
+        autoplay: {
+          delay: 1000,
+          disableOnInteraction: false,
+        },
+
+        spaceBetween: 10,
+        breakpoints: {
+          320: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          480: {
+            enabled: false,
+          },
+        },
+      });
+    });
+  }
+}
+
+customElements.define('as-seen-in', AsSeenIn);
